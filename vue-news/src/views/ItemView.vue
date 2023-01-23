4fc79ecbb83015f04a8f37fda44bd11b6aa84d75
Rabiori <ctangtangbbang@gmail.com>
2023-01-23 15:10:26 +0900

    라우터 트랜지션 적용

--- vue-news/src/views/ItemView.vue
+++ vue-news/src/views/ItemView.vue
@@ -1,17 +1,38 @@
 <template>
   <div>
-    <p>{{ itemInfo.title }}</p>
-    <div>{{ itemInfo.content }}</div>
+    <section>
+      <!-- 질문답 -->
+      <div class="user-container">
+        <div>
+          <i class="fas fa-user"></i>
+        </div>
+        <div class="user-desc">
+          <router-link v-bind:to="`/user/${fetchedItem.user}`">
+            {{ fetchedItem.user }}
+          </router-link>
+          <div class="time">
+            {{ fetchedItem.time_ago }}
+          </div>
+        </div>
+      </div>
+      <h2>{{ fetchedItem.title }}</h2>
+    </section>
+
+    <section>
+      <!-- 질문 댓글 -->
+      <div v-html="fetchedItem.content"></div>
+    </section>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
   computed: {
-    itemInfo() {
-      return this.$store.state.item;
-    },
+    ...mapGetters(["fetchedItem"]),
   },
+
   created() {
     const id = this.$route.params.id;
     this.$store.dispatch("FETCH_ITEM", id);
@@ -19,4 +40,22 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.user-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+}
+
+.fa-user {
+  font-size: 2.5rem;
+}
+
+.user-desc {
+  padding-left: 8px;
+}
+
+.time {
+  font-size: 0.7rem;
+}
+</style>
