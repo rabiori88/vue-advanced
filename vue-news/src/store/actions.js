9ef0d8388f2469f9bfadc63f6f1476647d30acd2
Rabiori <ctangtangbbang@gmail.com>
2023-01-29 15:47:25 +0900

    test

--- vue-news/src/store/actions.js
+++ vue-news/src/store/actions.js
@@ -18,13 +18,22 @@ export default {
         console.log(error);
       });
   },
-  FETCH_LIST({ commit }, pageName) {
-    return fetchList(pageName)
-      .then(({ data }) => {
-        commit("SET_LIST", data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  async FETCH_LIST({ commit }, pageName) {
+    try {
+      const response = await fetchList(pageName);
+      commit("SET_LIST", response.data);
+      // console.log(pageName + " " + response.data);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+
+    // return fetchList(pageName)
+    //   .then(({ data }) => {
+    //     commit("SET_LIST", data);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
   },
 };
